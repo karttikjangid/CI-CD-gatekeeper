941dc33e610267c59719c985f3b3dbb392a8b53f
karttikjangid <kartik.24bcs10259@sst.scaler.com>
2026-04-25 14:48:12 +0530

    testing for column change

--- test_drop.sql
+++ test_drop.sql
@@ -1,1 +1,5 @@
-DROP TABLE sample_data.ecommerce_db.shopify.raw_customer;
+-- Simulating a developer removing PII (email) without realizing 
+-- that the downstream Tier-1 customer features dashboard relies on it.
+
+ALTER TABLE sample_data.ecommerce_db.shopify.raw_customer 
+DROP COLUMN email;
