cc0a5bb952024271eac7a5232c44fb5feef1dcf5
Kartik <Kartik.24BCS10259@sst.scaler.com>
2026-04-26 15:39:35 +0530

    Delete test_drop.sql

--- test_drop.sql
+++
@@ -1,5 +0,0 @@
--- Simulating a developer removing PII (email) without realizing 
--- that the downstream Tier-1 customer features dashboard relies on it.
-
-ALTER TABLE sample_data.ecommerce_db.shopify.raw_customer 
-DROP COLUMN email;
