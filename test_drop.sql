9c6d0e559ebb7cdf66557e3fdb516a91936c3595
karttikjangid <kartik.24bcs10259@sst.scaler.com>
2026-04-26 18:59:38 +0530

    testing for video

--- test_drop.sql
+++ test_drop.sql
@@ -5,4 +5,4 @@ DROP COLUMN email;
 
 -- Adding a harmless change to prove the AST parser differentiates between nodes
 ALTER TABLE sample_data.ecommerce_db.shopify.raw_customer 
-ADD COLUMN last_audit_timestamp TIMESTAMP;
+ADD COLUMN last_audit_timestamp   TIMESTAMP;
