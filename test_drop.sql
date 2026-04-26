f0f19f2910b191d540bba7306017e3de54d0c5b1
karttikjangid <kartik.24bcs10259@sst.scaler.com>
2026-04-26 18:46:14 +0530

    test for video

---
+++ test_drop.sql
@@ -0,0 +1,8 @@
+-- AUTO-MEDIC TEST CASE: Destructive Schema Change
+-- Impact: High (Downstream ML Feature Store)
+ALTER TABLE sample_data.ecommerce_db.shopify.raw_customer 
+DROP COLUMN email;
+
+-- Adding a harmless change to prove the AST parser differentiates between nodes
+ALTER TABLE sample_data.ecommerce_db.shopify.raw_customer 
+ADD COLUMN last_audit_timestamp TIMESTAMP;
